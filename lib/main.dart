c5310ffcd18791628edc1624c010567726052c03
ramsony1 <62914223+ramsony1@users.noreply.github.com>
2020-05-17 07:02:34 -0400

    Theming the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:personalexpenses/widgets/user_transactions.dart';
+import 'package:personalexpenses/widgets/new_transaction.dart';
+import 'package:personalexpenses/widgets/transaction_list.com.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,26 +11,99 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        textTheme: GoogleFonts.quicksandTextTheme(
+          textTheme,
+        ).copyWith(
+          headline6: GoogleFonts.openSans(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          textTheme: GoogleFonts.openSansTextTheme(textTheme).copyWith(
+            headline6: GoogleFonts.openSans(
+              textStyle: textTheme.headline6,
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-//  String titleInput;
-//  String amountInput;
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+//    Transaction(
+//      id: 't1',
+//      title: 'New Shoes',
+//      amount: 69.99,
+//      date: DateTime.now(),
+//    ),
+//    Transaction(
+//      id: 't2',
+//      title: 'Weekly Grocery',
+//      amount: 16.53,
+//      date: DateTime.now(),
+//    ),
+//    Transaction(
+//      id: 't3',
+//      title: 'Internet Serveice',
+//      amount: 150.99,
+//      date: DateTime.now(),
+//    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTransaction = Transaction(
+      id: DateTime.now().toString(),
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _userTransactions.add(newTransaction);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Personal Expenses'),
+        title: Text(
+          'Personal Expenses',
+         // style: GoogleFonts.openSans(fontSize: 20),
+        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () => _startAddNewTransaction(context),
           )
         ],
       ),
@@ -42,13 +119,14 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransactions(),
+            TransactionList(_userTransactions),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
